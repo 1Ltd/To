2ba07d12b56cac881162d9ddb48509367088ddf4
1NE <151998806+1Ltd@users.noreply.github.com>
2025-10-15 15:32:49 +0530

    Update header.tsx

--- app/components/layout/header.tsx
+++ app/components/layout/header.tsx
@@ -33,7 +33,7 @@ export function Header({ hasSidebar }: { hasSidebar: boolean }) {
                 href="/"
                 className="pointer-events-auto inline-flex items-center text-xl font-medium tracking-tight"
               >
-                <ZolaIcon className="mr-1 size-4" />
+                <ToTIcon className="mr-1 size-4" />
                 {APP_NAME}
               </Link>
               {hasSidebar && isMobile && <HeaderSidebarTrigger />}
