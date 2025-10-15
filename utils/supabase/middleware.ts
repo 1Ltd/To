cbe35afbf7af15d750bd069fd4c1a86bb81d1983
1NE <151998806+1Ltd@users.noreply.github.com>
2025-10-15 17:48:44 +0530

    Update middleware.ts

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -8,14 +8,15 @@ export async function updateSession(request: NextRequest) {
       request,
     })
   }
+
+  let supabaseResponse = NextResponse.next({
+    request,
+  })
+
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
-    auth: {
-      persistSession: true,
-      detectSessionInUrl: true,
-      storage: localStorage,
       cookies: {
         getAll() {
           return request.cookies.getAll()
@@ -32,7 +33,7 @@ export async function updateSession(request: NextRequest) {
           )
         },
       },
-    },
+    }
   )
 
   // Do not run code between createServerClient and
